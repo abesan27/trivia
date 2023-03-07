bac2ddcd541dd95198a02a5af137c263d065d4ba
Albert Stan <albertstan30@gmail.com>
2023-03-07 14:23:42 +0000

    more questions

--- src/pages/api/questions.ts
+++ src/pages/api/questions.ts
@@ -21,7 +21,7 @@ let data: Question[] = [
     correctAnswer: "Jamaica",
   },
   {
-    question: "With which country does Frace share it's longest border?",
+    question: "With which country does France share it's longest border?",
     possibleAnswers: ["Netherlands", "Italy", "Spain", "Brazil"],
     correctAnswer: "Brazil",
   },
@@ -89,6 +89,137 @@ let data: Question[] = [
     question: "What is the joule a unit for?",
     possibleAnswers: ["Volume", "Mass", "Energy", "Acceleration"],
     correctAnswer: "Energy",
+  },
+  {
+    question: "What is the smallest planet in our solar system?",
+    possibleAnswers: ["Mercury", "Venus", "Earth", "Mars"],
+    correctAnswer: "Mercury",
+  },
+  {
+    question: "Who painted the Mona Lisa?",
+    possibleAnswers: ["Leonardo da Vinci", "Michelangelo", "Raphael", "Donatello"],
+    correctAnswer: "Leonardo da Vinci",
+  },
+  {
+    question: "What is the capital of Japan?",
+    possibleAnswers: ["Tokyo", "Kyoto", "Osaka", "Hiroshima"],
+    correctAnswer: "Tokyo",
+  },
+  {
+    question: 'Who wrote the novel "To Kill a Mockingbird"?',
+    possibleAnswers: ["Harper Lee", "Toni Morrison", "Maya Angelou", "Alice Walker"],
+    correctAnswer: "Harper Lee",
+  },
+  {
+    question: "What is the highest mountain in Africa?",
+    possibleAnswers: ["Mount Kilimanjaro", "Mount Kenya", "Mount Elgon", "Mount Meru"],
+    correctAnswer: "Mount Kilimanjaro",
+  },
+  {
+    question: "Who is the lead singer of the band Coldplay?",
+    possibleAnswers: ["Chris Martin", "Bono", "Adam Levine", "Justin Timberlake"],
+    correctAnswer: "Chris Martin",
+  },
+  {
+    question: "What is the largest organ in the human body?",
+    possibleAnswers: ["Liver", "Brain", "Skin", "Heart"],
+    correctAnswer: "Skin",
+  },
+  {
+    question: "Which country is home to the Great Barrier Reef?",
+    possibleAnswers: ["Australia", "Brazil", "South Africa", "Indonesia"],
+    correctAnswer: "Australia",
+  },
+  {
+    question: "What is the name of the famous scientist who developed the theory of relativity?",
+    possibleAnswers: ["Albert Einstein", "Isaac Newton", "Galileo Galilei", "Stephen Hawking"],
+    correctAnswer: "Albert Einstein",
+  },
+  {
+    question: "Which country won the 2018 FIFA World Cup?",
+    possibleAnswers: ["France", "Brazil", "Germany", "Spain"],
+    correctAnswer: "France",
+  },
+  {
+    question: "What is the rarest blood type?",
+    possibleAnswers: ["AB-", "B+", "A-", "O+"],
+    correctAnswer: "AB-",
+  },
+  {
+    question: "What is the smallest country in the world by land area?",
+    possibleAnswers: ["Vatican City", "Monaco", "Nauru", "Tuvalu"],
+    correctAnswer: "Vatican City",
+  },
+  {
+    question: "Which is the only continent that lies in all four hemispheres?",
+    possibleAnswers: ["Africa", "Antarctica", "Asia", "Europe"],
+    correctAnswer: "Africa",
+  },
+  {
+    question: "Who discovered radium and polonium?",
+    possibleAnswers: ["Marie Curie", "Albert Einstein", "Isaac Newton", "Charles Darwin"],
+    correctAnswer: "Marie Curie",
+  },
+  {
+    question: "What is the longest river in Europe?",
+    possibleAnswers: ["Volga", "Danube", "Elbe", "Rhine"],
+    correctAnswer: "Volga",
+  },
+  {
+    question: "What is the capital of Uzbekistan?",
+    possibleAnswers: ["Tashkent", "Samarkand", "Bukhara", "Andijan"],
+    correctAnswer: "Tashkent",
+  },
+  {
+    question: "What is the smallest bone in the human body?",
+    possibleAnswers: ["Stapes (in the ear)", "Femur", "Humerus", "Radius"],
+    correctAnswer: "Stapes (in the ear)",
+  },
+  {
+    question: 'Who wrote the novel "War and Peace"?',
+    possibleAnswers: ["Leo Tolstoy", "Fyodor Dostoevsky", "Anton Chekhov", "Mikhail Bulgakov"],
+    correctAnswer: "Leo Tolstoy",
+  },
+  {
+    question: "Who is the first woman to win a Nobel Prize?",
+    possibleAnswers: ["Marie Curie", "Rosalind Franklin", "Barbara McClintock", "Dorothy Hodgkin"],
+    correctAnswer: "Marie Curie",
+  },
+  {
+    question: "What is the most abundant gas in Earth's atmosphere?",
+    possibleAnswers: ["Nitrogen", "Oxygen", "Carbon dioxide", "Argon"],
+    correctAnswer: "Nitrogen",
+  },
+  {
+    question: "What is the currency of Brazil?",
+    possibleAnswers: ["Real", "Peso", "Dollar", "Euro"],
+    correctAnswer: "Real",
+  },
+  {
+    question: "What is the capital of France?",
+    possibleAnswers: ["Paris", "London", "Berlin", "Madrid"],
+    correctAnswer: "Paris",
+  },
+  {
+    question: "What is the name of the famous detective created by Sir Arthur Conan Doyle?",
+    possibleAnswers: ["Sherlock Holmes", "Hercule Poirot", "Miss Marple", "Nancy Drew"],
+    correctAnswer: "Sherlock Holmes",
+  },
+  {
+    question: "What is the largest country in the world by land area?",
+    possibleAnswers: ["Russia", "Canada", "China", "United States"],
+    correctAnswer: "Russia",
+  },
+  {
+    question: "What is the name of the largest ocean in the world?",
+    possibleAnswers: ["Pacific Ocean", "Atlantic Ocean", "Indian Ocean", "Arctic Ocean"],
+    correctAnswer: "Pacific Ocean",
+  },
+  {
+    question:
+      "What is the name of the iconic structure in Paris that is a popular tourist attraction?",
+    possibleAnswers: ["Eiffel Tower", "Big Ben", "Statue of Liberty", "Golden Gate Bridge"],
+    correctAnswer: "Eiffel Tower",
   },
 ];
 
